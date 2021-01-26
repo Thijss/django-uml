b23d3e3a4df1b3e8964a7ed7342b6386e3aa2ef0
Thijs Baaijen <Thijss@users.noreply.github.com>
2021-01-26 21:40:40 +0100

    Add settings.test and move some settings to settings.base

--- config/settings/base.py
+++ config/settings/base.py
@@ -8,6 +8,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 env = environ.Env(DEBUG=bool)
 env.read_env(str(BASE_DIR / ".env"))
 
+# SECRET_KEY and DEBUG are irrelevant since we're not running a web application
+SECRET_KEY = "SECRET-KEY-IS-IRRELEVANT-SINCE-THERE-IS-NO-WEBSERVER"
+DEBUG = True
+
+# UML DIRECTORY
+UML_EXPORTS_DIR = BASE_DIR / ".exports"
+UML_EXPORTS_DIR.mkdir(exist_ok=True)
+
+
 # Application definition
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
@@ -15,6 +24,8 @@ INSTALLED_APPS = [
     # LOCAL APPS
     "apps.commands",
     "apps.example",
+    # THIRD PARTY
+    "django_extensions",
 ]
 
 TEMPLATES = [
